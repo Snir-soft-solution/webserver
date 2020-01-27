d7615e00850f60687304cdaca1d3ec86839529d2
AdilsonLopesSnir <antonio.lopes@snir.co.ao>
2020-01-27 15:37:09 +0100

    New Version

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { ISservices } = require('./services/ISSERVICES');
 
-module.exports.ISservices = ISservices;
+module.exports.webserver = {
+    ISservices
+};
 // const isService = new ISservices(null, 'web')
 
 // console.log(isService.task.get({ table: 'allLinhaProduto', properties: 'designacao' }).then(e => {
